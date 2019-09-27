ee124d4b8ef430759181238a0f4d2808d72e9f66
Yaison <yaison1708@gmail.com>
2019-09-27 17:26:10 -0400

    Initial project files

---
+++ src/main/java/com/jobsity/App.java
@@ -0,0 +1,13 @@
+package com.jobsity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
